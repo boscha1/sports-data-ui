fa11d61d8f25556163f22d407a4a647efd4747b0
boscha1 <bosch.anthony7@gmail.com>
2022-12-30 23:30:49 -0500

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import {Route, Routes} from 'react-router-dom'
+import TeamsPage from "./components/team/TeamsPage";
 import TeamPage from "./components/team/TeamPage";
 
 function App() {
     return (
         <div className="container-fluid">
             <Routes>
-                <Route path="/" element={<TeamPage />} />
+                <Route path="/teams" element={<TeamsPage />} />
+                <Route path="/teams/:id" element={<TeamPage />} />
             </Routes>
         </div>
     )
